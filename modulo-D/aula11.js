8bf34f2327a32e41f0ea0d1c68c9e01eb1e24548
Samuel Lopes <140510892+SamuelLopesAnd@users.noreply.github.com>
2023-11-24 21:12:26 -0300

    comitei

---
+++ modulo-D/aula11.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log(`A velocidade do seu carro é  ${vel}km/h`)
+if (vel > 60){
+    console.log(`voce ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
